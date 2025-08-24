d0ca0fc1defdf17791b4090e6477fba14f0288a0
heaven-music <heaven.musicbot@gmail.com>
2025-08-24 11:58:11 +0530

    Add files via upload

---
+++ emojis.js
@@ -0,0 +1,19 @@
+module.exports = {
+    success: "<a:orange:1407988640327405640>",
+    error: "<:error:1408060256373837904>",
+    music: "<:OrangeDot:1407984210102194218>",
+    queue: "<a:orange_butterfly:1408002226579832914>",
+    info: "<a:MusicNotes:1407992551234867220>",
+    time: "<:Ticking:1407993902363054160>",
+    song: "<a:gaana:1408084383402819755>",
+    volume: "<a:sound:1408042819536289814>",
+    pause: "<:pause:1350022763493982308>",
+    play: "<a:play:1408021205641855019>",
+    skip: "<:skip:1350022757714493461>",
+    previous: "<:previous:1350022753922584576>",
+    stop: "<:stop:1350022750940561500>",
+    shuffle: "<a:shuffle:1408078553760661575>",
+    repeat: "<:Spotify_Repeat:1408043955412533249>",
+    artist: "<a:MusicNotes:1407989451333832795>",
+    loading: "<:loading:1350022748809723904>"
+}; 
