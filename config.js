e173c5ba70b758b6d73964cd2177d52f1bafea67
heaven-music <heaven.musicbot@gmail.com>
2025-08-24 11:53:18 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    prefix: '!',
+    nodes: [{
+        host : "panel.visionhost.cloud",
+        password : "krypton",
+        port : 2030,
+        source : false
+    }],
+    spotify: {
+        clientId: "a568b55af1d940aca52ea8fe02f0d93b",
+        clientSecret: "e8199f4024fe49c5b22ea9a3dd0c4789"
+    },
+    botToken: "MTQwNzg5Mjk1NzE1OTQ4OTYwOQ.GhJbCa.M4iXfHdRuvs0oHQXTOga3YN432jl6AJN-VFvPY",
+    embedColor: "#ff9d3b"
+};
